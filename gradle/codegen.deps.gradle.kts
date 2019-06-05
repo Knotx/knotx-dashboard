d93b49b65bb297c7b2d0771c01e168d312dca1e6
Tomasz Michalak <tomek.michalak@gmail.com>
2019-06-05 11:01:40 +0200

    Set 1.4.0 version

--- gradle/codegen.deps.gradle.kts
+++ gradle/codegen.deps.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
   "annotationProcessor"(platform("io.knotx:knotx-dependencies:${project.version}"))
   "annotationProcessor"(group = "io.vertx", name = "vertx-codegen", classifier = "processor")
   "annotationProcessor"(group = "io.vertx", name = "vertx-service-proxy", classifier = "processor")
-  "annotationProcessor"(group = "io.vertx", name = "vertx-rx-java2-gen")
 }
 
 tasks.named<JavaCompile>("compileJava") {
